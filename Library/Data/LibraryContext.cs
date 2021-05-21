ef3d53bea2952106a83197766d44bbdc1c1e2eb3
Alla <roi_mukh@mail.ru>
2021-05-21 16:11:34 +0300

    Web Page Books added

--- Library/Data/LibraryContext.cs
+++ Library/Data/LibraryContext.cs
@@ -32,7 +32,7 @@ namespace Library.Data
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 //optionsBuilder.UseSqlite("Data Source = C:\\Users\\chikwork\\Desktop\\АЛЛА\\проектБД\\Library.db");
-                optionsBuilder.UseSqlServer("Data Source = (localdb)\\mssqllocaldb; Database=Dblybrary; Trusted_Connection=True");
+                optionsBuilder.UseSqlServer("Data Source = CHIK\\SQLEXPRESS; Database=Dblybrary; Trusted_Connection=True");
             }
         }
 
--- Library/Startup.cs
+++ Library/Startup.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Library.Data;
+
 
 namespace Library
 {
@@ -24,7 +27,9 @@ namespace Library
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-        }
+            services.AddDbContext<LibraryContext>(options =>
+                    options.UseSqlServer("Data Source=CHIK\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True"));
+            }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
